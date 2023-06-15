1b8f0fc3ba8dbe8e0813f3130f448e7943d02e6c
nschlenker <noah.schlenker2002@gmail.com>
2023-06-15 09:14:51 +0200

    program interface definition

---
+++ Implementierung/rahmenprogramm.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+bool run_program(void){
+    /*Get user options adn trugger matrix exponentiation program.
+     *Returns:
+        - is_valid (_Bool): true if user options were vaild, else false*/
+    return true;
+}
+
+
+
+
+int main(void){
+    if (run_program()){
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+}
