825425be8bef0ad74e6977d74ff60639050af9ab
nschlenker <noah.schlenker2002@gmail.com>
2023-06-16 08:35:15 +0200

    add help / usage msg

--- Implementierung/rahmenprogramm.c
+++ Implementierung/rahmenprogramm.c
@@ -3,6 +3,20 @@
 #include <stdbool.h>
 
 
+const char* usage_msg = 
+    "Usage: %s [options]   Compute the square root of 2\n";
+
+const char* help_msg =
+    "Optional arguments:\n"
+    "  -V X   The version of the progam (default: X = 0)\n"
+    "  -B     Time the runtime\n"
+    "  -B X   Time the runtime with X repetitions (default: X = 1)\n"
+    "  -d X   Expected number of decimal places (default: X = 2)\n"
+    "  -h X   Expected number hexadecimal places (default: X = 2)\n"
+    "  -h     Show help message (this text) and exit\n"
+    "  --help     Show help message (this text) and exit\n";
+
+
 bool run_program(int argc, char *argv[]){
     /*Get user options adn trugger matrix exponentiation program.
      *Returns:
