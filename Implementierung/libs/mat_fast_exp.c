c3461f3cda2f956c2dac9f190ba99fa5dd21cd24
Leon <l.kniffki@t-online.de>
2023-06-19 11:37:27 +0200

    implemented optimized version of matrix power with compact matrix

--- Implementierung/libs/mat_fast_exp.c
+++ Implementierung/libs/mat_fast_exp.c
@@ -43,3 +43,33 @@ struct matrix4x4 powMatrix4x4(struct matrix4x4 a, uint32_t n) {
 
     return result;
 }
+
+/* Computes the power of a 4x4 matrix with following structure: | x-1  x  |
+ *                                                              |  x  x+1 |
+ * */
+struct cmp_matrix4x4 powCmpMatrix4x4(struct cmp_matrix4x4 a, uint32_t n) {
+    // The unit matrix is defined as x-1= 1, x = 0 and x+1 = 1
+    struct cmp_matrix4x4 result = {
+            bignumOfInt(1),
+            bignumOfInt(0),
+            bignumOfInt(1)
+    };
+
+    // First check manual to save one matrix multiplication at the end of the for loop
+    if (n & 1) {
+        result = mulCmpMatrix4x4(result, a);
+    }
+    n >>= 1;
+    // Squaring the matrix and multiply result by it if necessary
+    for (; n > 0; n >>= 1) {
+        // Square a
+        a = mulCmpMatrix4x4(a, a);
+
+        // If the least significant bit is set to 1, multiply result by a
+        if (n & 1) {
+            result = mulCmpMatrix4x4(result, a);
+        }
+    }
+
+    return result;
+}
--- Implementierung/libs/mat_fast_exp.h
+++ Implementierung/libs/mat_fast_exp.h
@@ -3,3 +3,5 @@
 struct bignum bignumOfInt(uint32_t);
 
 struct matrix4x4 powMatrix4x4(struct matrix4x4, uint32_t);
+
+struct cmp_matrix4x4 powCmpMatrix4x4(struct cmp_matrix4x4, uint32_t);
