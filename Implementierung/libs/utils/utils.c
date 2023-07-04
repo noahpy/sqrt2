3f14297127d57a2359043ddf84f0b96598fda751
nschlenker <noah.schlenker2002@gmail.com>
2023-07-04 13:13:14 +0200

    update utils.c

--- Implementierung/libs/utils/utils.c
+++ Implementierung/libs/utils/utils.c
@@ -4,8 +4,7 @@
 #include "../big_num/big_num.h"
 
 void print_bignum_dec(struct bignum *num){
-    uint64_t a = 4294967296 * (uint64_t)num->digits[1] + num->digits[0];
-    printf("%d\n", num->digits[0]);
+    (void) num;
 }
 
 void print_bignum_hex(struct bignum *num){
