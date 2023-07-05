8b06b5c76e747f44ffd864105f1d14e70a895c96
nschlenker <noah.schlenker2002@gmail.com>
2023-07-05 13:38:13 +0200

    bignum print dec

--- Implementierung/libs/utils/tests/test_utils.c
+++ Implementierung/libs/utils/tests/test_utils.c
@@ -1,15 +1,90 @@
 
-#include "../utils.h"
-#include <stdint.h>
+#define _GNU_SOURCE
 
-int main(){
-    // number: 74857857827867872 hex: 109F2D3_387A0CE0
-    uint32_t* digits = malloc(sizeof(uint32_t) * 2);
-    digits[0] = 0x387a0ce0;
-    digits[1] = 0x109F2D3;
-    struct bignum num = {digits, 2, 0};
-    char* buffer = malloc(sizeof(char) * 100);
-    print_bignum_dec(num, buffer, 100);
-    free(digits);
-    free(buffer);
+#include "../utils.h"
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int test_cases = 0;
+int test_passed = 0;
+
+void test_print_bignum_dec(struct bignum input, char *expected) {
+  test_cases++;
+  // read ouput on stdout
+  int stdout_bk; // is fd for stdout backup
+
+  stdout_bk = dup(fileno(stdout));
+
+  int pipefd[2];
+  pipe2(pipefd, 0); // O_NONBLOCK);
+
+  // What used to be stdout will now go to the pipe.
+  dup2(pipefd[1], fileno(stdout));
+
+  print_bignum_dec(&input, multiplicationBignum);
+
+  fflush(stdout); // flushall();
+  close(pipefd[1]);
+
+  dup2(stdout_bk, fileno(stdout)); // restore
+
+  char buf[strlen(expected) + 1];
+  read(pipefd[0], buf, strlen(expected));
+  buf[strlen(expected)] = 0;
+
+  // compare output with expected
+  if (strncmp(buf, expected, strlen(expected))) {
+    fprintf(stderr, "Test number %d failed: Expected\n\t%s\nbut got:\n\t%s\n", test_cases, expected, buf);
+    return;
+  }
+  fprintf(stderr, "Test passed: got %s\n", buf);
+  test_passed++;
+}
+
+int main() {
+  uint32_t *digits = malloc(4 * sizeof(uint32_t));
+  // 0x1_22333444_45555.544_44333221
+  digits[0] = 0x44333221;
+  digits[1] = 0x45555544;
+  digits[2] = 0x22333444;
+  digits[3] = 0x1;
+  struct bignum number = {digits, 4, 40};
+  test_print_bignum_dec(
+      number, "81684042822800725.2666656491646790527738630771636962890625");
+  free(digits);
+
+  // 0x1.7b72
+  digits = malloc(1 * sizeof(uint32_t));
+  digits[0] = 0x17b72;
+  struct bignum number2 = {digits, 1, 16};
+  test_print_bignum_dec(number2, "1.482208251953125");
+  free(digits);
+
+  // 83989_89e89a89_401bbc90_90fed902
+  digits = malloc(4 * sizeof(uint32_t));
+  digits[0] = 0x90fed902;
+  digits[1] = 0x401bbc90;
+  digits[2] = 0x89e89a89;
+  digits[3] = 0x83989;
+  struct bignum number3 = {digits, 4, 0};
+  test_print_bignum_dec(number3, "42705369154598132393320858769086722");
+  free(digits);
+
+  // 1.6A0
+  digits = malloc(1 * sizeof(uint32_t));
+  digits[0] = 0x16a0;
+  struct bignum number4 = {digits, 3, 12};
+  test_print_bignum_dec(number4, "1.4140625");
+
+  // print overall resulte
+  float success_rate = ((float)test_passed) / ((float)test_cases) * 100;
+  printf("PASSED: %d, FAILED: %d, SUCCESS RATE: %.1f%%\n", test_passed,
+         (test_cases - test_passed), success_rate);
+
+  return 0;
 }
--- Implementierung/libs/utils/utils.c
+++ Implementierung/libs/utils/utils.c
@@ -1,10 +1,153 @@
 
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "../big_num/big_num.h"
 
-void print_bignum_dec(struct bignum *num){
-    (void) num;
+void reverse_string(char* string, size_t len){
+    for(size_t i = 0; i < len / 2; i++){
+        char temp = string[i];
+        string[i] = string[len - i - 1];
+        string[len - i - 1] = temp;
+    }
+}
+
+char get_hex_value(char hex){
+    if(hex >= '0' && hex <= '9'){
+        return hex - '0';
+    }
+    if(hex >= 'A' && hex <= 'F'){
+        return hex - 'A' + 10;
+    }
+    if(hex >= 'a' && hex <= 'f'){
+        return hex - 'a' + 10;
+    }
+    fprintf(stderr, "Error: Invalid hex character\n");
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * @brief Converts a hex string to a decimal string
+ * 
+ * @param hex The hex string
+ * @param len The length of the hex string
+ * @param fraction_len The length of the fraction part of the resulting decimal string,
+ *        which is equal to the number of bits after the decimal point
+ * @return char* The decimal string
+ */
+char* hex_to_dec(char* hex, size_t len, size_t fraction_len){
+    // determine the length of the decimal string with magic number 1.204119983 (ln(16) / ln(10))
+    size_t string_len;
+    size_t fraction = (size_t) ceil(((double) len) * 0.204119983);
+    if(__builtin_add_overflow(len, fraction, &string_len)){
+        fprintf(stderr, "Error: Could not respresent the length of the decimal string\n");
+        exit(EXIT_FAILURE);
+    }
+    // allocate memory for the decimal string (with extra space for dot, zero and terminating null byte)
+    char* result = malloc(string_len + 3);
+    if(result == NULL){
+        fprintf(stderr, "Error: Could not allocate memory for the decimal string\n");
+        exit(EXIT_FAILURE);
+    }
+    // convert the hex string to decimal
+    size_t result_len = 0;
+    for(size_t i = 0; i < len; i++){
+        int carry = get_hex_value(hex[i]);
+        for(size_t j = 0; j < result_len; j++){
+            int digit = result[j] * 16 + carry;
+            result[j] = (char) (digit % 10);
+            carry = digit / 10;
+        }
+        while(carry){
+            result[result_len++] = (char) (carry % 10);
+            carry /= 10;
+        }
+    }
+    // format the result string
+    reverse_string(result, result_len);
+    for(size_t i = 0; i < result_len; i++){
+        result[i] += '0';
+    }
+
+    if(result_len < fraction_len){
+        fprintf(stderr, "Error: The fraction length is greater than the decimal length\n");
+        exit(EXIT_FAILURE);
+    }
+    size_t offset = (result_len - fraction_len) ? 0 : 1;
+
+    // add dot, zero and remove trailing zeros
+    if(fraction_len){
+        bool trailing = true;
+        size_t index = result_len - 1;
+        while(index >= result_len - fraction_len && index < result_len){
+            if(result[index] == '0' && trailing){
+                result[index] = '\0';
+            }
+            else{
+                trailing = false;
+                result[index + 1 + offset] = result[index];
+            }
+            index--;
+        }
+        result[index + 1 + offset] = '.';
+        if(!(result_len - fraction_len)){
+            result[0] = '0';
+        }
+    }
+
+    // add terminating null byte 
+    result[result_len + 1 + offset] = '\0';
+    return result;
+}
+
+
+char* bin_to_hex(struct bignum num){
+    size_t string_len;
+    if(__builtin_umull_overflow(num.size, 8, &string_len)){
+        fprintf(stderr, "Error: Could not represent the length of the hex string\n");
+        exit(EXIT_FAILURE);
+    }
+    char* result = malloc(string_len + 1);
+    if(result == NULL){
+        fprintf(stderr, "Error: Could not allocate memory for the hex string\n");
+        exit(EXIT_FAILURE);
+    }
+    char* current = result;
+    for (int i = num.size - 1; i > -1; i--){
+        sprintf(current, "%08x", num.digits[i]);
+        current += 8;
+    }
+    // add terminating null byte
+    result[string_len] = '\0';
+    return result;
+}
+
+
+void print_bignum_dec(struct bignum *num, struct bignum multiply(struct bignum, struct bignum)){
+    struct bignum result = *num;
+        if(num->fracSize){
+        size_t frac_hex = num->fracSize / 4;
+        struct bignum multiplier = bignumOfInt(10000);
+        for(size_t i = 0; i < frac_hex; i++){
+            struct bignum new_result = multiply(result, multiplier);
+            if(i > 0) free(result.digits);
+            result = new_result;
+        }
+        free(multiplier.digits);
+        // remove fraction zeros
+        shiftRight(&result, result.fracSize);  
+    }
+    char* hex_string = bin_to_hex(result);
+    char* dec_string = hex_to_dec(hex_string, strlen(hex_string), num->fracSize);
+    
+    printf("%s\n", dec_string);
+    free(hex_string);
+    free(dec_string);
+    if(num->fracSize) free(result.digits);
 }
 
 void print_bignum_hex(struct bignum *num){
