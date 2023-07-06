f2863e51df80e157a71b85c5f19663de1986227f
Christian Krinitsin <krc@in.tum.de>
2023-07-06 22:13:17 +0200

    add loop to main method

--- Implementierung/sqrt2.c
+++ Implementierung/sqrt2.c
@@ -6,18 +6,16 @@
 #include "libs/utils/utils.h"
 
 struct bignum sqrt2(size_t s){
-    struct cmp_matrix2x2 m = {
-        bignumOfInt(0),
-        bignumOfInt(1),
-        bignumOfInt(2)
-    };
+    for (int i = 1; i < 300; i++) {
+        struct cmp_matrix2x2 m = {
+            bignumOfInt(0),
+            bignumOfInt(1),
+            bignumOfInt(2)
+        };
 
-    struct cmp_matrix2x2 r = powCmpMatrix2x2(m, 300, multiplicationBignum);
-
-    /* print_bignum_dec(&r.x, multiplicationBignum); */
-    /* print_bignum_dec(&r.xm1, multiplicationBignum); */
-    /* print_bignum_dec(&r.xp1, multiplicationBignum); */
-    /*  */
-    divisionBignum(&r.x, &r.xp1, 200);
-    print_bignum_dec(&r.x, multiplicationBignum, true);
+        struct cmp_matrix2x2 r = powCmpMatrix2x2(m, i, multiplicationBignum);
+        divisionBignum(&r.x, &r.xp1, 200);
+        print_bignum_dec(&r.x, multiplicationBignum, true);
+        freeCmp2x2(r);
+    }
 }
