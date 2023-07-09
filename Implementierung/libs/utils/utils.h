8a69eff3bd612c384a169e6db2437e8b2eed444f
nschlenker <noah.schlenker2002@gmail.com>
2023-07-09 17:34:39 +0200

    add bintohex to header

--- Implementierung/libs/utils/utils.h
+++ Implementierung/libs/utils/utils.h
@@ -8,3 +8,5 @@ char* hex_to_dec(char* hex, size_t hex_size);
 void print_bignum_dec(struct bignum* num, struct bignum multiply(struct bignum, struct bignum), bool isSqrt2);
 
 void print_bignum_hex(struct bignum *num);
+
+char* bin_to_hex(struct bignum num);
