7bae9eca472a40f47451801ab29b3e098416b238
Christian Krinitsin <christian.krinitsin@tum.de>
2023-07-12 20:17:24 +0200

    runtime class performance

--- Implementierung/libs/big_num/runtime_class_performance/runtime_performance_big_num.c
+++ Implementierung/libs/big_num/runtime_class_performance/runtime_performance_big_num.c
@@ -5,29 +5,17 @@
 
 uint32_t *allocateDigits(size_t number);
 
-void generate_testset(unsigned int seed, size_t n, struct bignum array[], int maxsize) {
+void generate_testset(unsigned int seed, size_t n, struct bignum array[]) {
     srand(seed);
-    for (size_t i = 0; i < n; i++) {
-        size_t size = (rand() % maxsize) + 1;
-        uint32_t *digits = allocateDigits(size);
-        for (size_t j = 0; j < size; j++) {
-            digits[j] = (uint32_t) rand();
+    for (size_t i = 0; i < n; i=+2) {
+        uint32_t *digits1 = allocateDigits(i);
+        uint32_t *digits2 = allocateDigits(i);
+        for (size_t j = 0; j < i; j++) {
+            digits1[j] = (uint32_t) rand();
+            digits2[j] = (uint32_t) rand();
         }
-        array[i] = (struct bignum){digits, size, 0};
-    }
-}
-
-void generate_ascending(unsigned int seed, size_t n, struct bignum array[], int maxsize) {
-    srand(seed);
-    size_t stepsize = maxsize / n - 1;
-    size_t size = 1;
-    for (size_t i = 1; i < n; i++) {
-        uint32_t *digits = allocateDigits(size);
-        for (size_t j = 0; j < size; j++) {
-            digits[j] = (uint32_t) rand();
-        }
-        array[i] = (struct bignum){digits, size, 0};
-        size += stepsize;
+        array[i] = (struct bignum){digits1, i, 0};
+        array[i+1] = (struct bignum){digits2, i, 0};
     }
 }
 
@@ -78,24 +66,22 @@ int main () {
     // Inspired by the example from slide 4
     // https://gra.caps.in.tum.de/b/9a48e342ee6b6a950c3b5102a9e18ddc9b5ccb5c750110e8ee507345d263fb6a/v8-0.pdf
     size_t n_mul = 100;
-    size_t n_add = 50000;
-    size_t n_div = 300;
     struct bignum tests[2 * n_mul];
     printf("%s\n", "Generating test bignums, this may take some time...");
-    generate_ascending(1234, 2 * n_mul, tests, 10000);
+    generate_testset(1234, 2 * n_mul, tests);
     printf("Running tests for %zu multiplications\n", n_mul);
 
-    // Normal Multiplication
-    struct bignum results_nmul[n_mul];
-    struct timespec start_nmul;
-    clock_gettime(CLOCK_MONOTONIC, &start_nmul);
     for (size_t i = 0; i < 2 * n_mul; i += 2) {
-        results_nmul[i/2] = multiplicationBignum(tests[i], tests[i+1]);
+      // Normal Multiplication
+      struct bignum results_nmul[n_mul];
+      struct timespec start_nmul;
+      clock_gettime(CLOCK_MONOTONIC, &start_nmul);
+      results_nmul[i/2] = multiplicationBignum(tests[i], tests[i+1]);
+      struct timespec end_nmul;
+      clock_gettime(CLOCK_MONOTONIC, &end_nmul);
+      double time_nmul = end_nmul.tv_sec - start_nmul.tv_sec + 1e-9 * (end_nmul.tv_nsec - start_nmul.tv_nsec);
+      printf("Normal multiplication with size %ld took %f seconds.\n", i/2, time_nmul);
     }
-    struct timespec end_nmul;
-    clock_gettime(CLOCK_MONOTONIC, &end_nmul);
-    double time_nmul = end_nmul.tv_sec - start_nmul.tv_sec + 1e-9 * (end_nmul.tv_nsec - start_nmul.tv_nsec);
-    printf("Normal multiplication took %f seconds.\n", time_nmul);
 
     freeArray(2 * n_mul, tests);
 }
