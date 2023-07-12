e645eefaadd176d2db0695fd920f061ad700fb3f
nschlenker <noah.schlenker2002@gmail.com>
2023-07-12 17:14:27 +0200

    karazuba experiment

---
+++ Implementierung/tests/record_runtime.py
@@ -0,0 +1,66 @@
+
+import subprocess
+import time
+import matplotlib.pyplot as plt
+import sys
+
+plt.ion()  # Enable interactive mode
+
+fig, ax = plt.subplots()
+
+TEST_RANGE = 10000
+STEP = 10
+ITERATION = 100
+
+times = []  # Store the count values
+precisions = []  # Store the i values
+
+
+
+for i in range(1, TEST_RANGE + 1, STEP):
+    counter = ITERATION
+    runtime = 0
+    while(counter):
+        start = time.perf_counter()
+        result = subprocess.run(['../main', f'-d{i}'], capture_output=True, text=True)
+        end = time.perf_counter()
+        runtime += (end - start)
+        counter -= 1
+    runtime /= ITERATION
+    errors = result.stderr
+    
+    if errors:
+        print(f"Received error messages when running ../main -d{i}:", errors)
+        break
+
+    times.append(runtime)
+    precisions.append(i)
+
+    ax.clear()
+
+    # Plot the count vs. iterations graph
+    ax.plot(precisions, times, label='Average runtime in correlation with demanded precision')
+    ax.set_xlabel('precision in decimal digits')
+    ax.set_ylabel(f'average runtime of {ITERATION} iterations in s')
+    ax.grid(True)
+
+    ax.legend()
+    plt.draw()
+    plt.pause(0.001)
+
+file_name = "runtime_graph.png"
+if len(sys.argv) >= 2:
+    file_name = sys.argv[1]
+# Save the final graph to a file
+plt.savefig(file_name)
+
+# Turn off interactive mode
+plt.ioff()
+
+# Display the graph
+plt.show()
+
+
+
+
+
