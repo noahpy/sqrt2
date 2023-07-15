891f8700685e60b36fe733c8d894c875df216dac
Leon <l.kniffki@t-online.de>
2023-07-15 15:06:29 +0200

    added benchmarking to matrix multiplication

--- Implementierung/libs/matrix_mul/performance/performance_matrix_mul.c
+++ Implementierung/libs/matrix_mul/performance/performance_matrix_mul.c
@@ -5,13 +5,21 @@
 
 void generate_testset(unsigned int seed, size_t n, size_t stepsize, struct bignum array[][3]) {
     srand(seed);
-    for (size_t i = 0; i < n; i++) {
-        struct cmp_matrix2x2 base = {bignumOfInt(0), bignumOfInt(1), bignumOfInt(2)};
-        base = powCmpMatrix2x2(base, (rand() % 5000), multiplicationBignum);
-        array[i][0] = base.xm1;
-        array[i][1] = base.x;
-        array[i][2] = base.xp1;
+    struct cmp_matrix2x2 base = {bignumOfInt(0), bignumOfInt(1), bignumOfInt(2)};
+    struct cmp_matrix2x2 current = {bignumOfInt(0), bignumOfInt(1), bignumOfInt(2)};
+    struct cmp_matrix2x2 old;
+    for (size_t i = 0; i < n / stepsize; i++) {
+        while (current.x.size < (i * stepsize)) {
+            old = current;
+            current = mulCmpMatrix2x2(base, current, multiplicationBignum, additionBignum);
+            freeCmp2x2(old);
+        }
+        array[i][0] = current.xm1;
+        array[i][1] = current.x;
+        array[i][2] = current.xp1;
+        current = mulCmpMatrix2x2(base, current, multiplicationBignum, additionBignum);
     }
+    freeCmp2x2(base);
 }
 
 int compareBignum(struct bignum a, struct bignum b) {
@@ -48,16 +56,16 @@ static void write_file(const char *path, const char *string) {
 int main () {
     // Inspired by the example from slide 4
     // https://gra.caps.in.tum.de/b/9a48e342ee6b6a950c3b5102a9e18ddc9b5ccb5c750110e8ee507345d263fb6a/v8-0.pdf
-    size_t n = 50;
-    int iterations = 100;
-    size_t stepsize = 10;
-    struct bignum tests[2*(n/stepsize)][3];
+    size_t n = 5100;
+    size_t iterations = 10;
+    size_t stepsize = 100;
+    struct bignum tests[n/stepsize][3];
     printf("%s\n", "Generating test matrices, this may take some time...");
-    generate_testset(1234, 2*n, stepsize, tests);
-    printf("Running tests for %zu multiplications %d times: %zu multiplications in total\n", n / stepsize, iterations, n * iterations);
-    struct matrix2x2 normalset[2*(n/stepsize)];
-    struct cmp_matrix2x2 cmpset[2*(n/stepsize)];
-    for (size_t i = 0; i < 2*n; i++) {
+    generate_testset(1234, n, stepsize, tests);
+    printf("Running tests for %zu multiplications %zu times: %zu multiplications in total\n", n / stepsize, iterations, n * iterations);
+    struct matrix2x2 normalset[n/stepsize];
+    struct cmp_matrix2x2 cmpset[n/stepsize];
+    for (size_t i = 0; i < n/stepsize; i++) {
         normalset[i] = (struct matrix2x2) {tests[i][0], tests[i][1], tests[i][1], tests[i][2]};
         cmpset[i] = (struct cmp_matrix2x2) {tests[i][0], tests[i][1], tests[i][2]};
     }
@@ -65,21 +73,19 @@ int main () {
     struct matrix2x2 results_normal[n/stepsize];
     struct timespec start_normal;
     clock_gettime(CLOCK_MONOTONIC, &start_normal);
-    for (int i = 0; i < n/stepsize * 2; i += 2) {
-        results_normal[i/2] = mulMatrix2x2(normalset[i], normalset[i+1], multiplicationBignum, additionBignum);
+    for (size_t i = 0; i < n/stepsize; i++) {
+        results_normal[i] = mulMatrix2x2(normalset[i], normalset[i], multiplicationBignum, additionBignum);
     }
     struct timespec end_normal;
     clock_gettime(CLOCK_MONOTONIC, &end_normal);
     double time_normal = end_normal.tv_sec - start_normal.tv_sec + 1e-9 * (end_normal.tv_nsec - start_normal.tv_nsec);
     printf("Normal matrix multiplication took %f seconds.\n", time_normal);
 
-    struct cmp_matrix2x2 results_cmp[n*iterations];
+    struct cmp_matrix2x2 results_cmp[n/stepsize];
     struct timespec start_cmp;
     clock_gettime(CLOCK_MONOTONIC, &start_cmp);
-    for (int i = 0; i < iterations; i++) {
-        for (size_t j = 0; j < 2*n; j += 2) {
-            results_cmp[j/2 + n * i] = mulCmpMatrix2x2(cmpset[j], cmpset[j+1], multiplicationBignum, additionBignum);
-        }
+    for (size_t i = 0; i < n/stepsize; i++) {
+        results_cmp[i] = mulCmpMatrix2x2(cmpset[i], cmpset[i], multiplicationBignum, additionBignum);
     }
     struct timespec end_cmp;
     clock_gettime(CLOCK_MONOTONIC, &end_cmp);
@@ -87,7 +93,7 @@ int main () {
     printf("Compact matrix multiplication took %f seconds.\n", time_cmp);
 
     //free allocated space
-    for (size_t i = 0; i < 2*(n / stepsize); i++ ) {
+    for (size_t i = 0; i < n / stepsize; i++ ) {
         free(tests[i][0].digits);
         free(tests[i][1].digits);
         free(tests[i][2].digits);
@@ -110,7 +116,7 @@ int main () {
 
 
     printf("%s\n", "Generating benchmark results");
-    printf("This will take about %f seconds, because there are %d iterations\n", iterations * (time_normal + time_cmp),
+    printf("This will take about %f seconds, because there are %zu iterations\n", iterations * (time_normal + time_cmp),
            iterations);
     struct timespec start;
     struct timespec end;
@@ -125,7 +131,7 @@ int main () {
         // Calculate normal
         clock_gettime(CLOCK_MONOTONIC, &start);
         for (size_t j = 0; j < iterations; j++) {
-            free2x2(mulMatrix2x2(normalset[i], normalset[i+1], multiplicationBignum, additionBignum));
+            free2x2(mulMatrix2x2(normalset[i], normalset[i], multiplicationBignum, additionBignum));
         }
         clock_gettime(CLOCK_MONOTONIC, &end);
 
@@ -137,7 +143,7 @@ int main () {
         // Calculate compact
         clock_gettime(CLOCK_MONOTONIC, &start);
         for (size_t j = 0; j < iterations; j++) {
-            freeCmp2x2(mulCmpMatrix2x2(cmpset[i], cmpset[i+1], multiplicationBignum, additionBignum));
+            freeCmp2x2(mulCmpMatrix2x2(cmpset[i], cmpset[i], multiplicationBignum, additionBignum));
         }
         clock_gettime(CLOCK_MONOTONIC, &end);
 
